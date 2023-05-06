ed33afadaf10e87133e3ac17c5cb04defd3bc6ab
David Robertsson <david.robertsson@gmail.com>
2023-05-06 20:27:59 +0200

    Updated import function of dog data - Updated README
    The readme is appended with a sketch for a new schema.
    It seems to make sense, but further testing has to be done before certainty.
    
    The dog import will be moved into a future Data context.
    Data context will for now contain Dogs, but potentially more.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # DogBook
 
+## Http server
 To start your Phoenix server:
 
   * Run `mix setup` to install and setup dependencies
@@ -9,13 +10,39 @@ Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
 
 Ready to run in production? Please [check our deployment guides](https://hexdocs.pm/phoenix/deployment.html).
 
+## Database
+
+setup postgresql server. dev.config assumes /etc/hosts to point dog_book_db to a correct ip
+
+once the server is up, run:
+`mix ecto.create`
+
+
+# Models
+
+## Data
+
+mix phx.gen.live Meta Breeder breeders
+
+mix phx.gen.live Meta Color colors
+
+mix phx.gen.live Meta Champion champions
+
+mix phx.gen.live Data Dog dogs breed_id:references:breeds registry_uid:string name:string gender:enum:male:female  birth_date:date breed_specific:enum:bobtail:docked:measured coat:enum:short:long:broken size:enum:normal:dwarf:rabbit observe:boolean testicle_status:enum:ok:cryptochid  breeder_id:references:breeders color_number:references:colors
+**has_many of**
+parents:references:dog_parents
+champions:references:dog_champions
+
+mix phx.gen.schema Data.DogParents dog_parents dog_id:references:dogs parent_id:references:dogs
+
+
 ## Setup Meta data - breeds
 
 ``` bash
 iex -S mix
 ```
 
-in iex repl
+In iex repl
 
 ```elixir
 DogBook.Meta.BreedImport.process_breed()
--- lib/dog_book/meta/dog_import.ex
+++ lib/dog_book/meta/dog_import.ex
@@ -9,31 +9,55 @@ defmodule DogBook.Meta.DogImport do
   # Check if we can work with ranges.
   @dog_format %{
     0 => [:breed, :number],
-    1 => :dog_id,
+    1 => :registry_number,
     2 => :name,
     3 => :gender,
-    4..9 => :championships,
+    4..9 => [:championships, :number],
     10 => :birth_date,
-    11 => nil, #Breed specific data
-    12 => :fur,
+    11 => :breed_specific,
+    12 => :coat,
     13 => :size,
     14 => :observe,
     15 => :testicle_status,
-    16 => :father_id,
-    17 => :mother_id,
-    18 => :breeder_id,
-    19 => :color_number
+    16 => [:parents, :father_id],
+    17 => [:parents, :mother_id],
+    18 => [:breeder, :uid],
+    19 => [:color, :number]
+  }
+
+  @gender_mappings %{"H" => :male, "T" => :female}
+  @breed_specific_mappings %{"K" => :docked, "S" => :bobtail, "M" => :measured}
+  @coat_mappings %{"K" => :short, "L" => :long, "S" => :broken}
+  @size_mappings %{"N" => :normal, "D" => :dwarf, "K" => :rabbit}
+  @observation_mappings %{"S" => true, "" => false}
+  @testicle_mappings %{"G" => :ok, "K" => :cryptochid}
+
+  @format_mappings %{
+    3 => @gender_mappings,
+    11 => @breed_specific_mappings,
+    12 => @coat_mappings,
+    13 => @size_mappings,
+    14 => @observation_mappings,
+    15 => @testicle_mappings,
   }
 
   def dog_read(line) do
     list = String.split(line, "\t")
 
+    # Format mappings is only used in the is_integer(idx) case.
     dog =
       Enum.reduce(@dog_format, %{},
         fn {idx, k}, acc ->
           cond do
             is_integer(idx) and !is_nil(k) ->
-              Map.put(acc, k, Enum.at(list, idx))
+              val =
+                if Map.has_key?(@format_mappings, idx) do
+                  data_key = Enum.at(list, idx)
+                  @format_mappings[idx][data_key]
+                else
+                  Enum.at(list, idx)
+                end
+              Map.put(acc, k, val)
             is_struct(idx) ->
               Map.put(acc, k,
                 Enum.reduce(idx, [],
